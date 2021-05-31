d74304fb3529299a8ab8eaedb9258edbaefb04cc
Sergey Romanov <nashe.dobrov@gmail.com>
2021-05-31 22:04:31 +0300

    v0.994

--- src/components/TextElement.js
+++ src/components/TextElement.js
@@ -8,6 +8,18 @@ export const TextElement = ({ playingElem, index, elem, lang, playExactElement }
     setWidth(thisEl.current.clientWidth)
   }, [thisEl])
 
+  const spaceDevider = (string) => {
+    string = string.split(' ')
+    var stringArray = new Array()
+    for (var i = 0; i < string.length; i++) {
+      stringArray.push(string[i])
+      if (i != string.length - 1) {
+        stringArray.push(' ')
+      }
+    }
+    return stringArray
+  }
+
   return (
     <span
       className='text-item'
@@ -18,6 +30,18 @@ export const TextElement = ({ playingElem, index, elem, lang, playExactElement }
       style={{ background: playingElem === index ? 'lightgray' : 'white', cursor: 'pointer' }}
     >
       {' '}
+      {/* {spaceDevider(elem[lang]).map((e) => {
+        return (
+          <span
+            onClick={() => {
+              console.log(e)
+            }}
+            className={'textElementForHover'}
+          >
+            {e}
+          </span>
+        )
+      })} */}
       {elem[lang]}
     </span>
   )
