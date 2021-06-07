99e82ff4206eb366a61998f0673abb3a5ea94560
Sergey <nashe.dobrov@gmail.com>
2021-06-07 17:04:58 +0300

    v0.554 alpha

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useReducer, useState } from 'react'
 import { AudioEditor } from './pages/AudioEditor.js'
 import { reducer } from './context/reducer'
 import { AppContext } from './context/AppContext.js'
@@ -7,9 +7,12 @@ import { Toolbar } from './components/Toolbar.js'
 
 export const App = () => {
   const [appState, appDispatch] = useReducer(reducer, {})
+  const [projectName, setProjectName] = useState('testProject')
+
+  console.log(appState)
 
   return (
-    <AppContext.Provider value={{ appState, appDispatch }}>
+    <AppContext.Provider value={{ appState, appDispatch, projectName }}>
       <Toolbar>
         <div className={'wrapper'}>
           <AudioEditor />
